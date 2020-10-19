b43ba44028bc4cf972b93fa63105a1e0e182de2a
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-20 02:07:07 +0530

    [abhineet] add : ability to display welcome statement on AddressBookMain.java

---
+++ src/main/java/com/capgemini/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.capgemini.addressbooksystem;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AddressBookMain {
+	private static final Logger log = LogManager.getLogger(AddressBookMain.class);
+
+	public static void main(String[] args) {
+		log.info("Welcome to Address Book Program");
+	}
+}
