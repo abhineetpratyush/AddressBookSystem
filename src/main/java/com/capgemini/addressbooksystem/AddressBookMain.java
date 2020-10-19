11e5cfb29c30dcb4a920efb530820f2e25ba10bc
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-20 02:11:37 +0530

    [abhineet] add : ability to create a contact entry through the use of OOP concepts

--- src/main/java/com/capgemini/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/capgemini/addressbooksystem/AddressBookMain.java
@@ -6,7 +6,31 @@ import org.apache.logging.log4j.Logger;
 public class AddressBookMain {
 	private static final Logger log = LogManager.getLogger(AddressBookMain.class);
 
+	private String firstName;
+	private String lastName;
+	private String address; 
+	private String state;
+	private int zip;
+	private long phoneNo;
+	private String emailId;
+
+	public AddressBookMain(String firstName, String lastName, String address, String state, int zip, long phoneNo, String emailId) { 
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + firstName + " " + lastName + " Address: " + address;
+	}
+
 	public static void main(String[] args) {
-		log.info("Welcome to Address Book Program");
+		AddressBookMain contact = new AddressBookMain("Abhineet", "Pratyush", "EPIP, Bengaluru", "Karnataka", 123456, 1234567891, "xyz@gmail.com");
+		log.info(contact);
 	}
 }
